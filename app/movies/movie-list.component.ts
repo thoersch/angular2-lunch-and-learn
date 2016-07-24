7a57a0228dd4e21c86e6571c5c926bdd9aecbc22
Tyler Hoersch <thoersch@gmail.com>
2016-07-24 10:47:33 -0500

    updated movies

--- app/movies/movie-list.component.ts
+++ app/movies/movie-list.component.ts
@@ -25,5 +25,37 @@ export class MovieListComponent {
         "director": "Sidney Lumet",
         "image": "http://ia.media-imdb.com/images/M/MV5BODQwOTc5MDM2N15BMl5BanBnXkFtZTcwODQxNTEzNA@@._V1._SX40_CR0,0,40,54_.jpg",
         "rating": 8.9
+    },
+    {
+        "id": 3,
+        "title": "Star Trek: The Motion Picture",
+        "year": 1979,
+        "director": "Robert Wise",
+        "image": "http://ia.media-imdb.com/images/M/MV5BNzE4MTY1MTMyN15BMl5BanBnXkFtZTcwMzk5MzI4OA@@._V1._SX40_CR0,0,40,54_.jpg",
+        "rating": 6.4
+    },
+    {
+        "id": 4,
+        "title": "Ghostbusters",
+        "year": 1984,
+        "director": "Ivan Reitman",
+        "image": "http://ia.media-imdb.com/images/M/MV5BMTkxMjYyNzgwMl5BMl5BanBnXkFtZTgwMTE3MjYyMTE@._V1._SX40_CR0,0,40,54_.jpg",
+        "rating": 7.8
+    },
+    {
+        "id": 5,
+        "title": "Bloodsport",
+        "year": 1988,
+        "director": "Newt Arnold",
+        "image": "http://ia.media-imdb.com/images/M/MV5BMTc0MzQ5Mzk2NV5BMl5BanBnXkFtZTcwOTY1OTEyMQ@@._V1._SX40_CR0,0,40,54_.jpg",
+        "rating": 6.8
+    },
+    {
+        "id": 6,
+        "title": "Conan the Destroyer",
+        "year": 1984,
+        "director": "Richard Fleischer",
+        "image": "http://ia.media-imdb.com/images/M/MV5BMTM2NTEwODA3M15BMl5BanBnXkFtZTcwNTk2NjEzNA@@._V1._SX40_CR0,0,40,54_.jpg",
+        "rating": 5.8
     }];
 }
